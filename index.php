87b38f0e10293d09871d1c14da2c4612122957e2
xypecho <xypecho@163.com>
2017-10-13 21:35:54 +0800

    文件路径搭建
    Signed-off-by: xypecho <xypecho@163.com>

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>极简新风尚</title>
+	<link rel="stylesheet" type="text/css" href="public/css/layui.css">
+	<link rel="stylesheet" type="text/css" href="home/css/index.css">
+	<script type="text/javascript" src="public/js/jquery-3.2.1.min.js"></script>
+	<script type="text/javascript" src="public/js/layer.js"></script>
+</head>
+<body>
+	<div class="container">
+		<div class="content">
+			<div class="head">
+				<input type="text" name="search" class="layui-input" placeholder="SEARCH">
+			</div>
+		</div>
+	</div>  
+</body>
+</html>
